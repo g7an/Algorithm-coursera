fbb9a096415d88ff84e54f9a576f9a38da2fd11d
Shuyao Tan <sygloria.tan@gmail.com>
2020-12-09 00:40:50 +0800

    add fast collinear method in O(n^2logn) time

--- Documents/Algorithm/collinear/FastCollinearPoints.java
+++ Documents/Algorithm/collinear/FastCollinearPoints.java
@@ -16,73 +16,57 @@ public class FastCollinearPoints {
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
         final int ptSize = points.length;
-        List<LineSegment> resSegment = new ArrayList<>();
-        if (points.length == 0) {
-            throw new IllegalArgumentException("Input is null");
-        }
-        for (Point p : points) {
-            if (p == null) {
-                throw new IllegalArgumentException("Input contains null");
-            }
-        }
+        checkNull(points);
         Point[] pts = points.clone();
         Arrays.sort(pts);
-
-        for (int i = 0; i < ptSize - 1; i++) {
+        checkDup(pts, ptSize);
+        List<LineSegment> resSegment = new ArrayList<>();
+        for (int i = 0; i < ptSize; i++) {
             Point p = pts[i];
             // deep copy of sorted points
             Point[] sortedPoints = pts.clone();
             Arrays.sort(sortedPoints, p.slopeOrder());
-            for (Point pt : sortedPoints) {
-                StdOut.println(i + ": " + pt);
-            }
-            ArrayList<Point> collinearPts = new ArrayList<>();
+            // for (Point pt : sortedPoints) {
+            //     StdOut.println(i + ": " + pt);
+            // }
 
             // double slope = p.slopeTo(sortedPoints[i]);
-            for (int j = 1; j < ptSize; j++) {
-                double slope = p.slopeTo(sortedPoints[j]);
-                StdOut.println("slope: " + slope);
-                collinearPts.add(sortedPoints[j]);
-                for (int k = j + 1; k < ptSize; k++) {
-                    if (p.slopeTo(sortedPoints[k]) == slope) {
-                        collinearPts.add(sortedPoints[k]);
-                        StdOut.println(
-                                "p " + p + " , p j " + sortedPoints[j] + "p k" + sortedPoints[k]);
-                        StdOut.println("now size: " + collinearPts.size());
-                    }
-                    if (collinearPts.size() >= 3
-                            && p.compareTo(collinearPts.get(0)) < 0 && isUnique(resSegment, p,
-                                                                                collinearPts
-                                                                                        .get(collinearPts
-                                                                                                     .size()
-                                                                                                     - 1))) {
-                        StdOut.println(
-                                "p: " + p + " last " + collinearPts.get(collinearPts.size() - 1));
-                        resSegment
-                                .add(new LineSegment(p, collinearPts.get(collinearPts.size() - 1)));
-                    }
+            int n = 1;
+            while (n < ptSize) {
+                ArrayList<Point> collinearPts = new ArrayList<>();
+                double slope = p.slopeTo(sortedPoints[n]);
+                // StdOut.println("slope: " + slope);
+                do {
+                    collinearPts.add(sortedPoints[n++]);
+                } while (n < ptSize && p.slopeTo(sortedPoints[n]) == slope);
+                if (collinearPts.size() >= 3
+                        && p.compareTo(collinearPts.get(0)) < 0) {
+                    resSegment.add(new LineSegment(p, collinearPts.get(collinearPts.size() - 1)));
                 }
-
-
             }
             ls = resSegment.toArray(new LineSegment[0]);
-            for (LineSegment l : ls) {
-                StdOut.println(l);
+        }
+    }
+
+    private void checkNull(Point[] points) {
+        if (points.length == 0)
+            throw new IllegalArgumentException("input is null");
+        for (Point p : points) {
+            if (p == null) {
+                throw new IllegalArgumentException("input contains null");
             }
         }
     }
 
-
-    private boolean isUnique(List<LineSegment> resSegment, Point p, Point point) {
-        LineSegment testSeg = new LineSegment(p, point);
-        for (LineSegment res : resSegment) {
-            if (res == testSeg) {
-                return false;
+    private void checkDup(Point[] pts, int ptSize) {
+        if (ptSize > 1) {
+            for (int i = 0; i < ptSize - 1; i++) {
+                if (pts[i].compareTo(pts[i + 1]) == 0) {
+                    throw new IllegalArgumentException("input element duplicates");
+                }
             }
         }
-        return true;
     }
-
 
     // the number of line segments
     public int numberOfSegments() {
@@ -90,9 +74,9 @@ public class FastCollinearPoints {
     }
 
     // the line segments
-    // public LineSegment[] segments() {
-    //
-    // }
+    public LineSegment[] segments() {
+        return ls.clone();
+    }
 
     public static void main(String[] args) {
         Point[] pts = new Point[8];
@@ -107,5 +91,8 @@ public class FastCollinearPoints {
         FastCollinearPoints fcp = new FastCollinearPoints(pts);
 
         StdOut.println("Number of Segments: " + fcp.numberOfSegments());
+        for (LineSegment l : fcp.segments()) {
+            StdOut.println(l);
+        }
     }
 }
